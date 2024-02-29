c817b5f36a27cc539dcf1a6488f5657766b799db
Luiz Fernandes <luizfernandes.sistemas@gmail.com>
2024-02-29 16:26:35 -0400

    fix: Added add-ons in dealer service

--- request/requestDealer.service.js
+++ request/requestDealer.service.js
@@ -1,20 +1,48 @@
+const responseFeedbackService = require("../request/responseFeedback.service")
+
+const SUPPORTED_CONTENT_TYPE = {
+    JSON: 'application/json'
+}
+
 module.exports = {
     async init(req, res, next) {
         req.response = {
-            body: {},
-            
-            header: { status: 200, contentType: 'application/json' },
+            feedback: {
+                action: responseFeedbackService.getActions().VALIDATED,
+                field: 'api-access'
+            },
+            meta: {
+                contentType: SUPPORTED_CONTENT_TYPE.JSON,
+                error: null,
+            },
+            header: { 
+                status: 200 
+            },
+            body: {
+
+            },
         }
 
         return next()
     },
 
     async end(req, res, next) {
+        res.contentType(req.response.meta.contentType);
 
+        if (req.response.meta.contentType !== SUPPORTED_CONTENT_TYPE.JSON)
+            return res.end(req.response.body);
+
+        req.response.header.feedback = req.response.feedback;
+        
         res.status(parseInt(req.response.header.status));
+
+        let responseObj = {
+            header: req.response.header,
+            body: req.response.body
+        };
 
         console.log(">>> Resposta ");
 
-        return res.json(req.response.body);
+        return res.json(responseObj);
     }
 }
