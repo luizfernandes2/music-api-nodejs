711db4d364578787c86f75a93f2cfe9fb4c52657
Luiz Fernandes <luizfernandes.sistemas@gmail.com>
2024-02-29 16:27:41 -0400

    fix: Corrected validation field

--- validation/fieldsParsing.service.js
+++ validation/fieldsParsing.service.js
@@ -1,35 +1,49 @@
+const removeAccents = require('remove-accents');
+
+const MAPPING = {
+    'C': 'C',
+    'D': 'D',
+    'E': 'E',
+    'F': 'F',
+    'G': 'G',
+    'A': 'A',
+    'B': 'B',
+    'DO': 'C',
+    'RE': 'D',
+    'MI': 'E',
+    'FA': 'F',
+    'SOL': 'G',
+    'LA': 'A',
+    'SI': 'B'
+};
+
 module.exports = {
     nameNote: (name) => {
-        let nameNote = `${name}`.toUpperCase().trim();
+        let nameNote = `${removeAccents.remove(name)}`.toUpperCase().trim();
         
-        switch (nameNote) {
-            case 'DO':
-            case 'RE':
-            case 'MI':
-            case 'FA':
-            case 'SOL':
-            case 'LA':
-            case 'SI':
-                return nameNote;
-            default:
-                return null;
-        }
-    },
-
-    note: (data) => {
-        let note = `${data}`.toUpperCase().trim();
-
-        switch (note) {
-            case 'C':
-            case 'D':
-            case 'E':
-            case 'F':
-            case 'G':
-            case 'A':
-            case 'B':
-                return note;
-            default:
-                return null;
+        let parserNote = MAPPING[nameNote];
+        
+        if (parserNote) {
+            return parserNote;
+        } else {
+            return null;
         }
     }
+
+    // note: (data) => {
+    //     let note = `${data}`.toUpperCase().trim();
+
+    //     switch (note) {
+    //         case 'C':
+    //         case 'D':
+    //         case 'E':
+    //         case 'F':
+    //         case 'G':
+    //         case 'A':
+    //         case 'B':
+    //             return note;
+    //         default:
+    //             return null;
+    //     }
+    // }
 }
