739a72d9fbe38c0c009dbfded3b4c86f7a7f0351
Luiz Fernandes <luizfernandes.sistemas@gmail.com>
2024-02-29 16:29:03 -0400

    fix: Added scale listing function

--- source/note/controller/note.controller.js
+++ source/note/controller/note.controller.js
@@ -1,14 +1,36 @@
 const noteService = require("../service/note.service");
-
+const responseFeedbackService = require("../../../request/responseFeedback.service")
+const { end } = require("../../../request/requestDealer.service")
 const fieldsParsingService = require("../../../validation/fieldsParsing.service")
+const fieldsValidatorService = require("../../../validation/fieldsValidator.service")
 
 module.exports = {
     async listScale(req, res, next){
-        console.log("Scale")
+        let note = fieldsParsingService.nameNote(req.body.note)
         
-        let service = await noteService.listScale()
+        if (note === null) {
+            req.response.feedback = responseFeedbackService.newFeedback({ error: note });
+            return end(req, res);
+        }
 
-        return service
+        note = fieldsValidatorService.note(note)
+        
+        if (note === null) {
+            req.response.feedback = responseFeedbackService.newFeedback({ action: responseFeedbackService.getActions().INVALID, field: 'note' });
+            return end(req, res);
+        }
+        
+        let scale = await noteService.listScale(note)
+        
+        if(scale.error){
+            req.response.feedback = responseFeedbackService.newFeedback({ action: responseFeedbackService.getActions().INVALID, field: 'note' });
+            return end(req, res);
+        }
+
+        req.response.feedback = responseFeedbackService.newFeedback({ action: responseFeedbackService.getActions().READ, field: 'scale' });
+        req.response.body.scale = scale;
+
+        return next();
     },
 
     async listChord(req, res, next){
--- source/note/service/note.service.js
+++ source/note/service/note.service.js
@@ -1,7 +1,18 @@
-module.exports = {
-    async listScale(){
-        console.log("SERVICE LIST SCALE")
+function majorScale(nota) {
+    let scale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];;
 
+    let index = scale.indexOf(nota);
+
+    let seq = scale.slice(index).concat(scale.slice(0, index));
+
+    return seq;
+}
+
+module.exports = {
+    async listScale(note){
+        let scale = majorScale(note)
+
+        return { scale: scale.join(', ') };
     },
 
     async listChord(){
